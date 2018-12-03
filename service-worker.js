75dd969ff1434653a3676f8dd654fac5296b28a5
Vinicius Niche Correa <viniciusnichecorrea@gmail.com>
2018-12-02 23:10:23 -0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vniche/vniche.github.io/precache-manifest.bb41ad8f61b24665af7aeda310e2e564.js"
+  "/precache-manifest.67598dfb05c6e54d2d9e6088cc70c51c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/vniche/vniche.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
