40caee918d932866b08e37b5520339d784bb2eb1
Vinicius Niche Correa <viniciusnichecorrea@gmail.com>
2018-12-03 00:33:33 -0200

    Updates

---
+++ precache-manifest.71856d320586d3104fdd304711c1c148.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "4212391f168f59072e8ba8aa4b13048f",
+    "url": "/static/media/logo.4212391f.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "025c689732664a48a747",
+    "url": "/static/js/main.025c6897.chunk.js"
+  },
+  {
+    "revision": "05855c013765e597b760",
+    "url": "/static/js/1.05855c01.chunk.js"
+  },
+  {
+    "revision": "025c689732664a48a747",
+    "url": "/static/css/main.f3e1c019.chunk.css"
+  },
+  {
+    "revision": "d2d850311cc8f5346f63fbf9da379c78",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.67598dfb05c6e54d2d9e6088cc70c51c.js"
+  "/precache-manifest.71856d320586d3104fdd304711c1c148.js"
 );
 
 workbox.clientsClaim();
