771b5255170556da261fc5beac716a814895622d
Vinicius Niche Correa <viniciusnichecorrea@gmail.com>
2018-12-04 00:28:50 -0200

    Updates

--- precache-manifest.0d44744efbe613fc83f3045dc26b5d65.js
+++ precache-manifest.0d44744efbe613fc83f3045dc26b5d65.js
@@ -12,19 +12,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "025c689732664a48a747",
-    "url": "/static/js/main.025c6897.chunk.js"
+    "revision": "6a73d27eb57cb8054927",
+    "url": "/static/js/main.6a73d27e.chunk.js"
   },
   {
-    "revision": "05855c013765e597b760",
-    "url": "/static/js/1.05855c01.chunk.js"
+    "revision": "8ca0f8fc876507fdafb7",
+    "url": "/static/js/1.8ca0f8fc.chunk.js"
   },
   {
-    "revision": "025c689732664a48a747",
+    "revision": "6a73d27eb57cb8054927",
     "url": "/static/css/main.f3e1c019.chunk.css"
   },
   {
-    "revision": "d2d850311cc8f5346f63fbf9da379c78",
+    "revision": "14d46aa60159a3e0c856f41a13a5f660",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71856d320586d3104fdd304711c1c148.js"
+  "/precache-manifest.0d44744efbe613fc83f3045dc26b5d65.js"
 );
 
 workbox.clientsClaim();
